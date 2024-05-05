9474ef77b5b6cb7a23640a12adab0b86246972eb
Daniel Lee <leedh7878@hotmail.com>
2024-05-04 17:28:40 -0700

    revert

---
+++ Bike_Store_Relational_Database_SQL/src/Schema.sql
@@ -0,0 +1,89 @@
+CREATE TABLE Brands(
+    brand_id INTEGER PRIMARY KEY,
+    brand_name VARCHAR NOT NULL
+)
+
+CREATE TABLE Categories(
+    category_id INTEGER PRIMARY KEY,
+    category_name VARCHAR NOT NULL
+)
+
+CREATE TABLE Customers(
+    customer_id INTEGER PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_nmae VARCHAR NOT NULL,
+    phone CHAR(14),
+    email VARCHAR NOT NULL,
+    street VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    state CHAR(2),
+    zipcode CHAR(5)
+)
+
+CREATE TABLE Order_Items(
+    order_id INTEGER PRIMARY KEY,
+    item_id INTEGER,
+    product_id INTEGER,
+    list_price DECIMAL(6,2),
+    discount DECIMAL(1,2),
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(prodcut_id) REFERENCES Products(product_id)
+)
+
+CREATE TABLE Orders(
+    order_id INTEGER PRIMARY KEY, 
+    customer_id INTEGER NOT NULL,
+    order_status INTEGER NOT NULL,
+    order_date DATE,
+    required_date DATE,
+    shipped_date DATE,
+    store_id INTEGER NOT NULL,
+    staff_id INTEGER NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (store_id) REFERENCES Stores(store_id),
+    FOREIGN KEY (staff_id) REFERENCES Staffs(staff_id)
+
+)
+
+CREATE TABLE Products(
+    product_id INTEGER PRIMARY KEY,
+    product_name VARCHAR NOT NULL,
+    brand_id INTEGER,
+    category_id INTEGER,
+    model_year YEAR,
+    list_price DECIMAL(6,2),
+    FOREIGN KEY(brand_id) REFERENCES Brands(brand_id),
+    FOREIGN KEY(category_id) REFERENCES Categories(category_id)
+)
+
+CREATE TABLE Staffs(
+    staff_id INTEGER PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR,
+    phone CHAR(14),
+    active BIT(2) DEFAULT 1,
+    store_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY(store_id) REFERENCES Stores(store_id),
+    FOREIGN KEY(manager_id) REFERENCES Staffs(staff_id)
+)
+
+CREATE TABLE Stocks(
+    store_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER DEFAULT 0,
+    FOREIGN KEY(store_id) REFERENCES Stores(store_id),
+    FOREIGN KEY(product_id) REFERENCES Products(product_id)
+)
+
+CREATE TABLE Stores(
+    store_id INTEGER PRIMARY KEY,
+    store_name VARCHAR NOT NULL,
+    phone CHAR(14),
+    email VARCHAR,
+    street VARCHAR,
+    city VARCHAR,
+    state CHAR(2),
+    zip_code CHAR(5)
+)
